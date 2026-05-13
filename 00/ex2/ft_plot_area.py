6fb351c18f9c7efa6989ab37a2624103264ae5b0
iammehdib <iammehdib@protonmail.com>
2026-05-13 20:58:26 +0200

    feat(ft_plot_area): init code

---
+++ 00/ex2/ft_plot_area.py
@@ -0,0 +1,5 @@
+def ft_plot_area() -> None:
+    length_input: str = input("Enter length: ")
+    width_input: str = input("Enter width: ")
+    result: int = int(length_input) * int(width_input)
+    print(f"Plot area: {result}")
