698a3df0c2c2427bf19b4e4310cf9f1f5fddb052
iammehdib <iammehdib@protonmail.com>
2026-06-12 19:12:05 +0200

    refactor(module02/ex0): make short print for ValueError

--- module02/ex0/ft_first_exception.py
+++ module02/ex0/ft_first_exception.py
@@ -8,9 +8,10 @@ def test_temperature():
     for value in ['25', 'abc']:
         print(f"Input data is '{value}'")
         try:
-            print(f"Temperature is now {input_temperature(value)}°C")
-        except ValueError:
-            print("Caught input_temperature error: invalid literal for int() with base 10")
+            print(f"Temperature "
+                  f"is now {input_temperature(value)}°C")
+        except ValueError as e:
+            print(f"Caught input_temperature error: {e}")
         print()
 
     print("All tests completed - program didn't crash!")
