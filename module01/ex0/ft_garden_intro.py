2d461d0f1e4e0d1b381e91f0d5b30884f8d3b29f
iammehdib <iammehdib@protonmail.com>
2026-06-18 17:28:24 +0200

    fix(module01/ex0): don't use class PDF doesn''t allow use already Class

--- module01/ex0/ft_garden_intro.py
+++ module01/ex0/ft_garden_intro.py
@@ -1,16 +1,10 @@
-class Plant:
-    def __init__(self, name: str, height: int, age: int) -> None:
-        self.name = name
-        self.height = height
-        self.age = age
-
-
 if __name__ == "__main__":
-    plant = Plant("Rose", 35, 30)
-    print(f"""
-    === Welcome to My Garden ===
-    Plant: {plant.name}
-    Height: {plant.height}cm
-    Age: {plant.age} days
-    === End of Program ===
-    """)
+    name: str = "Rose"
+    height: int = 25
+    age: int = 30
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("=== End of Program ===")
