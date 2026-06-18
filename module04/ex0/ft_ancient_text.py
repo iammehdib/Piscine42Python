f60c7dd60eaffb6d0ba0578795343910b4893bf1
iammehdib <iammehdib@protonmail.com>
2026-06-18 18:47:41 +0200

    refactor(module04/ex0): improve some exceptions

--- module04/ex0/ft_ancient_text.py
+++ module04/ex0/ft_ancient_text.py
@@ -2,11 +2,11 @@ import sys
 import typing
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        print('=== Cyber Archives Recovery ===')
-        file_path: str = sys.argv[1]
-        print(f"Accessing file '{file_path}'")
-        try:
+    try:
+        if len(sys.argv) == 2:
+            print('=== Cyber Archives Recovery ===')
+            file_path: str = sys.argv[1]
+            print(f"Accessing file '{file_path}'")
             file: typing.IO = open(file_path, 'r')
             print('---')
             print()
@@ -15,7 +15,14 @@ if __name__ == '__main__':
             print('---')
             file.close()
             print(f"File '{file_path}' closed.")
-        except (FileNotFoundError, PermissionError) as e:
-            print(f"Error opening file '{file_path}': {e}")
-    else:
-        print('Usage: ft_ancient_text.py <file>')
+        else:
+            raise ValueError(f"Usage: {sys.argv[0]} <file>")
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+    except (KeyboardInterrupt):
+        print("\nKeyboard interrupt by user")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error opening file '{file_path}': {e}")
+        sys.exit(1)
