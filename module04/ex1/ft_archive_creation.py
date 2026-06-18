1983453adf144b9bc678e9b1f97427c84b2dae04
iammehdib <iammehdib@protonmail.com>
2026-06-18 19:12:24 +0200

    feat(module04/ex1): add some except

--- module04/ex1/ft_archive_creation.py
+++ module04/ex1/ft_archive_creation.py
@@ -2,43 +2,60 @@ import sys
 import typing
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        print('=== Cyber Archives Recovery ===')
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError(f"Usage: {sys.argv[0]} <file>")
+
+        print('=== Cyber Archives Recovery & Preservation ===')
         file_path: str = sys.argv[1]
         print(f"Accessing file '{file_path}'")
         try:
             file: typing.IO = open(file_path, 'r')
             file_content: str = file.read()
-            print('---')
-            print()
-            print(file_content)
-            print()
-            print('---')
             file.close()
-            print(f"File '{file_path}' closed.")
+        except OSError as e:
+            print(f"Error opening file '{file_path}': {e}")
+            sys.exit(1)
 
-            new_file_content: str = ''
-            for line in file_content.split('\n'):
-                if line == '':
-                    continue
-                new_file_content += line + "#\n"
-            print('Transform data:')
-            print('---')
-            print()
-            print(new_file_content)
-            print()
-            print('---')
+        print('---')
+        print()
+        print(file_content, end='')
+        print()
+        print('---')
+        print(f"File '{file_path}' closed.")
 
-            new_file_path: str = input('Enter new file name (or empty): ')
-            if new_file_path != '':
-                print(f"Saving data to '{new_file_path}'")
+        new_file_content: str = ''
+        for line in file_content.split('\n'):
+            if line == '':
+                continue
+            new_file_content += line + "#\n"
+        print('Transform data:')
+        print('---')
+        print()
+        print(new_file_content, end='')
+        print()
+        print('---')
+
+        new_file_path: str = input('Enter new file name (or empty): ')
+        if new_file_path:
+            print(f"Saving data to '{new_file_path}'")
+            try:
                 new_file: typing.IO = open(new_file_path, 'w')
                 new_file.write(new_file_content)
                 new_file.close()
                 print(f"Data saved in file '{new_file_path}'.")
-            else:
-                print('Not saving data.')
-        except (FileNotFoundError, PermissionError) as e:
-            print(f"Error opening file '{file_path}': {e}")
-    else:
-        print('Usage: ft_ancient_text.py <file>')
+            except OSError as e:
+                print(f"Error opening file '{new_file_path}': {e}")
+                sys.exit(1)
+        else:
+            print('Not saving data.')
+
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt by user")
+        sys.exit(1)
+    except EOFError:
+        print("\nEOF error")
+        sys.exit(1)
