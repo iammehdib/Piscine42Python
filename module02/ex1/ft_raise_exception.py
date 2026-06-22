46fdd14bd2ddeeba21e32f781fed687d9b265481
iammehdib <iammehdib@protonmail.com>
2026-06-22 20:37:57 +0200

    fix(module02/ex1): make good exception handling like and fix ValueError message

--- module02/ex1/ft_raise_exception.py
+++ module02/ex1/ft_raise_exception.py
@@ -1,26 +1,24 @@
 def input_temperature(temp_str: str) -> int:
-    return int(temp_str)
+    int_value: int = int(temp_str)
+    if int_value > 40:
+        raise ValueError(
+            f"{int_value}°C is too hot for plants (max 40°C)"
+        )
+    if int_value < 0:
+        raise ValueError(
+            f"{int_value}°C is too cold for plants (min 0°C)"
+        )
+    return int_value
 
 
-def test_temperature():
-    print('=== Garden Temperature ===')
+def test_temperature() -> None:
+    print('=== Garden Temperature Checker ===')
     print()
 
     for value in ['25', 'abc', '100', '-50']:
         print(f"Input data is '{value}'")
         try:
-            int_value = input_temperature(value)
-            if int_value > 40:
-                print(
-                    f"Caught input_temperature error: "
-                    f"{int_value}°C "
-                    f"is too hot for plants (max 40°C)"
-                )
-            if int_value < 0:
-                print(
-                    f"Caught input_temperature error: {int_value}°C is "
-                    f"too cold for plants (min 0°C)"
-                )
+            print(f"Temperature is now {input_temperature(value)}°C")
         except ValueError as e:
             print(f"Caught input_temperature error: {e}")
         print()
