3ef310c147d3838046bcb9153259c230ea3a0d8a
iammehdib <iammehdib@protonmail.com>
2026-07-26 23:04:18 +0200

    refactor(battle): make function for this exercice

--- module07/battle.py
+++ module07/battle.py
@@ -1,29 +1,34 @@
-from ex0 import FlameFactory, AquaFactory
+from ex0 import AquaFactory, CreatureFactory, FlameFactory
 
-print('Testing factory')
-flame_factory = FlameFactory()
-flame = flame_factory.create_base()
-print(flame.describe())
-print(flame.attack())
-pyro = flame_factory.create_evolved()
-print(pyro.describe())
-print(pyro.attack())
-print('')
 
-print('Testing factory')
-aqua_factory = AquaFactory()
-aqua = aqua_factory.create_base()
-print(aqua.describe())
-print(aqua.attack())
-torra = aqua_factory.create_evolved()
-print(torra.describe())
-print(torra.attack())
-print('')
+def test_factory(factory: CreatureFactory) -> None:
+    print("Testing factory")
+    for creature in (factory.create_base(), factory.create_evolved()):
+        print(creature.describe())
+        print(creature.attack())
 
-print('Testing battle')
-print(flame.describe())
-print(' vs.')
-print(aqua.describe())
-print(' fight!')
-print(flame.attack())
-print(aqua.attack())
+
+def test_battle(first: CreatureFactory, second: CreatureFactory) -> None:
+    print("Testing battle")
+    challenger = first.create_base()
+    opponent = second.create_base()
+    print(challenger.describe())
+    print(" vs.")
+    print(opponent.describe())
+    print(" fight!")
+    print(challenger.attack())
+    print(opponent.attack())
+
+
+if __name__ == "__main__":
+    try:
+        flame_factory = FlameFactory()
+        aqua_factory = AquaFactory()
+
+        test_factory(flame_factory)
+        print()
+        test_factory(aqua_factory)
+        print()
+        test_battle(flame_factory, aqua_factory)
+    except Exception as e:
+        print(f"Error: {e}")
